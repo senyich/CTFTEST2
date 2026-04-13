b731a76a8fc45c01a9ce59c56d6b29591a4a76aa
bumbarash <stepanovd4n@gmail.com>
2026-04-13 09:38:20 +0000

    init update bank

--- bank/cyberBank/src/imageGen.ts
+++ bank/cyberBank/src/imageGen.ts
@@ -11,7 +11,6 @@ class Image {
 
         await this.generateRandomImage(imagePath);
         await this.encodeImage(imagePath, content);
-        ХУЙ ХУЙ ХУЙ ХУЙ ХУЙ ХУЙ
         return imagePath.split("/")[5];
     }
 
