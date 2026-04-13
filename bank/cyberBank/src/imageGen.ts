629558ad01c42a7bcd75388b396cf086d4be63c9
senyich <senyacm@gmail.com>
2026-04-13 16:35:43 +0700

    update imageGen

--- bank/cyberBank/src/imageGen.ts
+++ bank/cyberBank/src/imageGen.ts
@@ -11,7 +11,7 @@ class Image {
 
         await this.generateRandomImage(imagePath);
         await this.encodeImage(imagePath, content);
-
+        ХУЙ ХУЙ ХУЙ ХУЙ ХУЙ ХУЙ
         return imagePath.split("/")[5];
     }
 
